0df1362a446a8bcdd5797b76407c31a5806845ec
Yaroslavkap <74808706+Yaroslavkap@users.noreply.github.com>
2021-09-30 09:46:50 +0300

    Add files via upload

---
+++ lab2/tlinkedlist.cpp
@@ -0,0 +1,96 @@
+#include"rectangle.h"
+#include"tlinkedlist.h"
+#include<iostream>
+using namespace std;
+
+
+
+
+
+
+
+    bool list::is_empty() {
+        return first == nullptr;
+    }
+
+    void list::push_back(Rectangle _val) {
+        Node* p = new Node(_val);
+        if (is_empty()) {
+            first = p;
+            last = p;
+            return;
+        }
+        last->next = p;
+        last = p;
+    }
+
+    void list::print() {
+        if (is_empty()) return;
+        Node* p = first;
+        while (p) {
+            cout << p->val << " ";
+            p = p->next;
+        }
+        cout << endl;
+    }
+
+    Node* list::find(Rectangle _val) {
+        Node* p = first;
+        while (p && p->val != _val) p = p->next;
+        return (p && p->val == _val) ? p : nullptr;
+    }
+
+    void list::remove_first() {
+        if (is_empty()) return;
+        Node* p = first;
+        first = p->next;
+        delete p;
+    }
+
+    void list::remove_last() {
+        if (is_empty()) return;
+        if (first == last) {
+            remove_first();
+            return;
+        }
+        Node* p = first;
+        while (p->next != last) p = p->next;
+        p->next = nullptr;
+        delete last;
+        last = p;
+    }
+
+    void list::remove(Rectangle _val) {
+        if (is_empty()) return;
+        if (first->val == _val) {
+            remove_first();
+            return;
+        }
+        else if (last->val == _val) {
+            remove_last();
+            return;
+        }
+        Node* slow = first;
+        Node* fast = first->next;
+        while (fast && fast->val != _val) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        if (!fast) {
+            cout << "This element does not exist" << endl;
+            return;
+        }
+        slow->next = fast->next;
+        delete fast;
+    }
+
+    Node* list::operator[] (const int index) {
+        if (is_empty()) return nullptr;
+        Node* p = first;
+        for (int i = 0; i < index; i++) {
+            p = p->next;
+            if (!p) return nullptr;
+        }
+        return p;
+    }
+
