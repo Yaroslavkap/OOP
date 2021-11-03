1651c49447467f03d392edf3713d3394938c124b
Yaroslavkap <74808706+Yaroslavkap@users.noreply.github.com>
2021-11-03 13:55:14 +0300

    Add files via upload

---
+++ lab2/main.cpp
@@ -0,0 +1,44 @@
+#include"rectangle.h"
+#include "tlinkedlist_i.h"
+#include "tlinkedlist.h"
+#include"point.h"
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	Point a;
+	a.setX(1);
+	a.setY(1);
+	Point b(2, 2);
+	Point c(3, 3);
+	Point d(4, 4);
+
+	Rectangle rc(c, a, b, d);
+	Rectangle rc1(a, b, c, d);
+	Rectangle rc2(d, b, c, d);
+
+	cout << b << endl;
+	cout << rc << endl;
+	
+	TLinkedList list;
+	list.InsertFirst(rc);
+	list.InsertFirst(rc1);
+	list.InsertLast(rc2);
+	cout << list << endl;
+	list.RemoveFirst();
+	list.RemoveLast();
+	cout << list.Length() << endl;
+	cout << list << endl;
+	cout << list.Empty() << endl;
+	list.RemoveLast();
+	cout << list.Empty() << endl;
+
+
+	TLinkedListItem I(rc, NULL);
+	//cout << I << endl;
+
+	return 0;
+
+}
